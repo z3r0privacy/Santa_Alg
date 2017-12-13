7452ce764034096b4a55434dddaf8f83a9e1357f
Fabian Furger <furgerfabian@hotmail.com>
2017-12-13 13:40:27 +0100

    Add neighbor that swaps with optimum within trip

--- neighbors/single-trip-neighbors.py
+++ neighbors/single-trip-neighbors.py
@@ -8,20 +8,26 @@ from neighbor import Neighbor
 from utils import memoize
 
 
-class SwapGiftsInTripNeighbor(Neighbor):
+class SwapRandomGiftsInTripNeighbor(Neighbor):
   def __init__(self, trips, log):
-    # when gifts to swap aren't specified, select them randomly
-    self.trip = trips[np.random.randint(len(trips))]
-    while len(self.trip) < 2:
+    # don't overwrite existing trip
+    if not hasattr(self, "trip"):
       self.trip = trips[np.random.randint(len(trips))]
-    self.first_gift = np.random.randint(len(self.trip))
-    self.second_gift = np.random.randint(len(self.trip))
-    while self.first_gift == self.second_gift:
+      while len(self.trip) < 2:
+        self.trip = trips[np.random.randint(len(trips))]
+
+    # don't overwrite existing gifts
+    if not hasattr(self, "first_gift"):
+      self.first_gift = np.random.randint(len(self.trip))
+
+    if not hasattr(self, "second_gift"):
       self.second_gift = np.random.randint(len(self.trip))
-    super(SwapGiftsInTripNeighbor, self).__init__(log)
+      while self.first_gift == self.second_gift:
+        self.second_gift = np.random.randint(len(self.trip))
+    super(SwapRandomGiftsInTripNeighbor, self).__init__(log)
 
   def __str__(self):
-    return "{}-swap-{}-{}".format(int(self.trip[0][1]), self.first_gift, self.second_gift)
+    return "{}-random-swap-{}-{}".format(int(self.trip[0][1]), self.first_gift, self.second_gift)
 
   def _get_cost_of_swapping_adjacent(self, a, b, c, d, cumulative_weight_at_a, weight_at_b, weight_at_c):
     old_cost = utils.distance(a, b) * cumulative_weight_at_a + \
@@ -32,11 +38,9 @@ class SwapGiftsInTripNeighbor(Neighbor):
         utils.distance(b, d) * (cumulative_weight_at_a - weight_at_c - weight_at_b)
     return new_cost - old_cost
 
-  @property
-  @memoize
-  def cost_delta(self):
-    i = min(self.first_gift, self.second_gift)
-    j = max(self.first_gift, self.second_gift)
+  def _calculate_cost_of_swapping_items(self, first, second):
+    i = min(first, second)
+    j = max(first, second)
 
     # set up weights
     weight_diff = self.trip[i][utils.WEIGHT] - self.trip[j][utils.WEIGHT]
@@ -59,12 +63,12 @@ class SwapGiftsInTripNeighbor(Neighbor):
           cum_weight_before_i, weight_i, weight_j)
     else:
       # cost of the old segments around i/j
-      old_i = self.get_cost_of_tour_of_three(before_i, at_i, after_i, cum_weight_before_i, weight_i)
-      old_j = self.get_cost_of_tour_of_three(before_j, at_j, after_j, cum_weight_before_j, weight_j)
+      old_i = Neighbor.get_cost_of_tour_of_three(before_i, at_i, after_i, cum_weight_before_i, weight_i)
+      old_j = Neighbor.get_cost_of_tour_of_three(before_j, at_j, after_j, cum_weight_before_j, weight_j)
 
       # cost of the new segments around i/j
-      new_j = self.get_cost_of_tour_of_three(before_i, at_j, after_i, cum_weight_before_i, weight_j)
-      new_i = self.get_cost_of_tour_of_three(before_j, at_i, after_j, cum_weight_before_j + weight_diff, weight_i)
+      new_j = Neighbor.get_cost_of_tour_of_three(before_i, at_j, after_i, cum_weight_before_i, weight_j)
+      new_i = Neighbor.get_cost_of_tour_of_three(before_j, at_i, after_j, cum_weight_before_j + weight_diff, weight_i)
 
       # cost difference from weight between i and j (sub-self.trip between i+1..j-1)
       distance = 0
@@ -78,6 +82,12 @@ class SwapGiftsInTripNeighbor(Neighbor):
 
     return improvement
 
+
+  @property
+  @memoize
+  def cost_delta(self):
+    return self._calculate_cost_of_swapping_items(self.first_gift, self.second_gift)
+
   def apply(self):
     # self.log.debug("Applying {}".format(self))
 
@@ -89,4 +99,42 @@ class SwapGiftsInTripNeighbor(Neighbor):
     if self.VERIFY_COST_DELTA:
       new = utils.weighted_trip_length(self.trip[:, utils.LOCATION], self.trip[:, utils.WEIGHT])
       utils.verify_costs_are_equal(self.cost_delta, new-old)
+
+
+class OptimalSwapInRandomTripNeighbor(SwapRandomGiftsInTripNeighbor):
+  def __init__(self, trips, log, trip=None, first_gift=None):
+    if trip is not None:
+      # select random trip with at least 2 gifts
+      self.trip = trips[np.random.randint(len(trips))]
+      while len(self.trip) < 2:
+        self.trip = trips[np.random.randint(len(trips))]
+
+    if first_gift is not None:
+      # select the first gift to swap randomly
+      self.first_gift = np.random.randint(len(self.trip))
+
+    # don't assign second gift yet
+    self.second_gift = -1
+
+    super(OptimalSwapInRandomTripNeighbor, self).__init__(trips, log)
+
+  def __str__(self):
+    return "{}-optimal-swap-{}-{}".format(int(self.trip[0][1]), self.first_gift, self.second_gift)
+
+  @property
+  @memoize
+  def cost_delta(self):
+    minimum_cost = np.finfo(np.float64).max
+    self.second_gift = None
+
+    for i in range(len(self.trip)):
+      if i == self.first_gift:
+        # don't swap with self
+        continue
+      current_cost = self._calculate_cost_of_swapping_items(self.first_gift, i)
+      if current_cost < minimum_cost:
+        minimum_cost = current_cost
+        self.second_gift = i
+
+    return minimum_cost
 
